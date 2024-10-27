4edb6a5a0ea458a8f363ed84918acfeff4cffbb8
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-10-27 09:17:59 +0700

    feat: update App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import "./App.css";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState([
+    { id: 1, name: "Hoàng Lanh" },
+    { id: 2, name: "Yến Nhi" },
+    { id: 3, name: "Thị Hà" },
+  ]);
+
+
+  const todos = todoList.map((todo, index) => {
+    return <TodoItem name={todo.name} key={todo.id} />;
+  });
+
 
   return (
-    <div>
-      <p></p>
+    <div className="container">
+      <input
+        type="text"
+        name="add-new-task"
+        placeholder="Add new task"
+        className="task-input"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            const value = e.target.value;
+            console.log(value);
+            setTodoList([
+              ...todoList,
+              { id: crypto.randomUUID(), name: value }
+            ]);
+          }
+        }}
+      />
+      <div>{todos}</div>
     </div>
   );
 }
